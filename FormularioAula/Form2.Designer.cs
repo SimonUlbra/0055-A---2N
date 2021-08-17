8c552aa1d0d989c2698a53365da00b51a030a0d0
SimonUlbra <luissimon96@rede.ulbra.br>
2021-08-16 22:25:31 -0300

    Ajustado o Status do Texto para Concluído a atividade que estava em meu nome, caso alguém tenha alguma dúvida pode me chamar que tentamos descobrir

--- FormularioAula/Form2.Designer.cs
+++ FormularioAula/Form2.Designer.cs
@@ -35,20 +35,22 @@ namespace FormularioAula
             // 
             // tbLista
             // 
-            this.tbLista.Location = new System.Drawing.Point(50, 37);
+            this.tbLista.Location = new System.Drawing.Point(67, 46);
+            this.tbLista.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tbLista.Multiline = true;
             this.tbLista.Name = "tbLista";
-            this.tbLista.Size = new System.Drawing.Size(434, 149);
+            this.tbLista.Size = new System.Drawing.Size(577, 182);
             this.tbLista.TabIndex = 0;
             this.tbLista.Text = resources.GetString("tbLista.Text");
             this.tbLista.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(536, 384);
+            this.ClientSize = new System.Drawing.Size(715, 473);
             this.Controls.Add(this.tbLista);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form2";
             this.Text = "Trabalho de Pesquisa";
             this.Load += new System.EventHandler(this.Form2_Load);
