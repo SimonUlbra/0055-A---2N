79cad13d975e5bfff38849bd3bce22b09ad5176c
SimonUlbra <luissimon96@rede.ulbra.br>
2021-08-16 21:03:09 -0300

    Ajustando novo formulário pq o outro não tinha mais solução

--- Aula1/Aula1/Form2.cs
+++ Aula1/Aula1/Form2.cs
@@ -3,25 +3,16 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Aula1
 {
-    public partial class Form1 : Form
+    public partial class Form2 : Form
     {
-        public Form1()
+        public Form2()
         {
             InitializeComponent();
-        }
-
-   
-
-        private void btOK_Click_Click(object sender, EventArgs e)
-        {
-            tbLista = tbLista + "; " + tbNome.Text;
         }
     }
 }
--- Aula1/Aula1/Program.cs
+++ Aula1/Aula1/Program.cs
@@ -17,7 +17,7 @@ namespace Aula1
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form2());
         }
     }
 }
